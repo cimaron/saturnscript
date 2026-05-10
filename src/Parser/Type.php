6a639ae6981003f5a6996716666fcce2540398ef
Cimaron Shanahan <cshanahan@cimaron.net>
2026-05-09 18:58:00 -0700

    Add size tracking for types

--- src/Parser/Type.php
+++ src/Parser/Type.php
@@ -17,31 +17,53 @@ class Type {
 	public static $builtins = [];
 
 	public $name;
+	public $size;
 
 	public $class = false;
 	public $function = false;
 	
 	public $members = [];
 	
-	public function __construct($name) {
+	/**
+	 * @param   string   $name
+	 * @param   int      $size
+	 */
+	public function __construct($name, $size = 1) {
 		$this->name = $name;
+		$this->size = $size;
+	}
+
+	/**
+	 * Get memory size of type
+	 *
+	 * @return  int
+	 */
+	public function getSize() {
+
+		$size = $this->size;
+
+		foreach ($this->members as $member) {
+			$size += $member->getSize();
+		}
+
+		return $size;
 	}
 }
 
 
-Type::$builtins['void'] = new Type('void');
+Type::$builtins['void'] = new Type('void', 0);
 
 
-Type::$builtins['u8'] = new Type('u8');
-Type::$builtins['i8'] = new Type('i8');
+Type::$builtins['u8'] = new Type('u8', 1);
+Type::$builtins['i8'] = new Type('i8', 1);
 
-Type::$builtins['u16'] = new Type('u16');
-Type::$builtins['i16'] = new Type('i16');
+Type::$builtins['u16'] = new Type('u16', 2);
+Type::$builtins['i16'] = new Type('i16', 2);
 
-Type::$builtins['u32'] = new Type('u32');
-Type::$builtins['i32'] = new Type('i32');
+Type::$builtins['u32'] = new Type('u32', 4);
+Type::$builtins['i32'] = new Type('i32', 4);
 
-Type::$builtins['fx8'] = new Type('fx8');
-Type::$builtins['fx16'] = new Type('fx16');
-Type::$builtins['fx24'] = new Type('fx24');
+Type::$builtins['fx8'] = new Type('fx8', 4);
+Type::$builtins['fx16'] = new Type('fx16', 4);
+Type::$builtins['fx24'] = new Type('fx24', 4);
 
